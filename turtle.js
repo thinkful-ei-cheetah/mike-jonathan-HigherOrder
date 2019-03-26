95794cbc9ec585599a9a04d9394682f8ec31f56f
MikeJung90 <kpnba90@gmail.com>
2019-03-26 15:11:36 -0400

    add reduce

--- turtle.js
+++ turtle.js
@@ -3,6 +3,6 @@
 const turtleSteps = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
 
 turtleSteps.filter(step => (step[0]>-1 && (step[1]>-1)))
-    .map(step => step[0]+step[1])
-    .forEach(step => console.log(step));
+  .map(step => step[0]+step[1])
+  .forEach(step => console.log(step));
 
